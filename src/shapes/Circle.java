33eab16743144b8eae4031496aa9a813eddc540c
DeviSri2090 <130747900+DeviSri2090@users.noreply.github.com>
2025-06-09 17:22:34 +0530

    Update Circle.java

--- src/shapes/Circle.java
+++ src/shapes/Circle.java
@@ -6,18 +6,15 @@ public class Circle implements Shape {
     public Circle(double radius) {
         this.radius = radius;
     }
-
-    @Override
+    
     public double area() {
         return Math.PI * radius * radius;
     }
-
-    @Override
+    
     public double perimeter() {
         return 2 * Math.PI * radius;
     }
 
-    @Override
     public String getName() {
         return "Circle";
     }
